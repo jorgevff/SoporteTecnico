7e173c808b6edd781f68c120f63993623e4aa1e6
jorgevff <jvalladaresfontano@gmail.com>
2018-02-16 08:30:46 +0100

    Se eliminan las respuestas aleatorias para que no se repitan y finaliza piendo que empiece otra vez

--- Responder.java
+++ Responder.java
@@ -16,43 +16,40 @@ public class Responder
     private ArrayList<String> respuestasAleatorias;
     private HashMap<HashSet<String>, String> respuestas;
     private Random aleatorio;
-    
-    
+
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
-        
-        
+
         aleatorio = new Random();
         respuestas = new HashMap<>();
         respuestasAleatorias = new ArrayList<>();
-        
+
         respuestasAleatorias.add("Instrucción incorrecta");
         respuestasAleatorias.add("Pensando...");
         respuestasAleatorias.add("Indica mejor el problema");
         respuestasAleatorias.add("Eso estodo amigos!!");
         //creamos conjuntos
-        
+
         HashSet<String> conjunto1 = new HashSet<>();
         conjunto1.add("menu");
         conjunto1.add("archivo");
-        
+
         HashSet<String> conjunto2 = new HashSet<>();
         conjunto2.add("reinicio");
         conjunto2.add("reseteo");
-        
+
         HashSet<String> conjunto3 = new HashSet<>();
         conjunto3.add("problema");
         conjunto3.add("menu");
-        
+
         HashSet<String> conjunto4 = new HashSet<>();
         conjunto4.add("power");
         conjunto4.add("bucle");
         conjunto4.add("of");
-        
-        
+
         respuestas.put(conjunto1, "Prueba con la primera pestaña");
         //porque con la primera en mayuscula no funciona?
         respuestas.put(conjunto2, "Reinicia por segunda vez");
@@ -67,19 +64,26 @@ public class Responder
     public String generateResponse(HashSet<String> palabraClave)
     {
         String respuesta = null;
-       
+
         respuesta = respuestas.get(palabraClave);
         /**String palabraIntroducidaPorTeclado = "";
         for(String palabra : palabraClave){
-          palabraIntroducidaPorTeclado = palabra;  
+        palabraIntroducidaPorTeclado = palabra;  
         }
-        **/
+         **/
 
         if(respuesta == null){
-            int posicionAleatoria = aleatorio.nextInt(respuestasAleatorias.size());
-            respuesta = respuestasAleatorias.get(posicionAleatoria);
+            // int posicionAleatoria = aleatorio.nextInt(respuestasAleatorias.size());
+            //respuesta = respuestasAleatorias.get(posicionAleatoria)
+            if(respuestasAleatorias.size() > 0){
+                //remove elimina el objeto y lo devuelve por lo que no hace falta extraerlo con el metodo get
+                respuesta = respuestasAleatorias.remove(aleatorio.nextInt(respuestasAleatorias.size()));
+            }
+            else{
+                respuesta = "No entiendo la pregunta, vuelva a empezar";
+            }
         }
-        
+
         return respuesta;
     }
 }
